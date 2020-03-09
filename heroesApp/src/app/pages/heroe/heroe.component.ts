00aebbbd5905ccd95dc428472dcc9c74d7ed81eb
roger26p <rogerdavidp55@gmail.com>
2020-03-08 21:52:52 -0300

    continuacion de la seccion

--- heroesApp/src/app/pages/heroe/heroe.component.ts
+++ heroesApp/src/app/pages/heroe/heroe.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroeModel } from 'src/app/models/heroe.model';
+import { NgForm } from '@angular/forms';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroe',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroeComponent implements OnInit {
 
-  constructor() { }
+  heroe: HeroeModel = new HeroeModel();
+
+  constructor(private heroesService: HeroesService) { }
 
   ngOnInit() {
   }
 
+  guardar(form: NgForm){
+    if(form.invalid){
+      console.log('Formulario no valido');
+      return;
+    }
+
+    if(this.heroe.id){
+      this.heroesService.actualizarHeroe(this.heroe)
+      .subscribe(resp =>{
+        console.log(resp)
+      });
+    }
+    else{
+      this.heroesService.crearHeroe(this.heroe)
+      .subscribe(resp =>{
+        console.log(resp)
+      });
+    }
+  }
 }
